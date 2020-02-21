1477d1e6b7b0d240ce69ff487fa8cfdb6412df38
lyeoni <lyeoni.g@gmail.com>
2020-02-21 17:30:37 +0900

    feat: add tokenization.py

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ PyTorch Implementation of OpenAI GPT
 
 ## Quick Start
 ### 0. Install PreNLP library
-PreNLP is Preprocessing Library for Natural Language Processing. Using this, we will load the corpus, and sentencepiece tokenizer.
+PreNLP is Preprocessing Library for Natural Language Processing. It provides sentencepiece tokenizer.
 ```
 $ pip install prenlp
 ```
@@ -15,16 +15,22 @@ $ pip install prenlp
 
 #### Building vocab based on your corpus
 ```
-$ python vocab.py --corpus <YOUR_CORPUS> --prefix <VOCAB_NAME> --vocab_size 16000
+$ python vocab.py --corpus <YOUR_CORPUS> --prefix <VOCAB_NAME> --vocab_size <YOUR_VOCAB_SIZE>
 ```
 
-or you can easily download WikiText-103 corpus using below command, and build vocab based on this.
+or you can download WikiText-103 corpus using below command, and build vocab based on this.
 ```
 $ python -c "import prenlp; prenlp.data.WikiText103()"
 $ ls .data/wikitext-103
 wiki.test  wiki.train  wiki.valid
 $ python vocab.py --corpus .data/wikitext-103/wiki.train --prefix wiki103
 ```
+
+### 2. Unsupervised pre-training
+
+
+### 3. Supervised fine-tuning
+
 
 ### References
 - [Improving Language Understandingby Generative Pre-Training](https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/language-unsupervised/language_understanding_paper.pdf)
---
+++ tokenization.py
@@ -0,0 +1,127 @@
+"""Code from
+    - https://github.com/lyeoni/nlp-tutorial/blob/master/translation-transformer/tokenization.py
+    - https://github.com/lyeoni/nlp-tutorial/blob/master/text-classification-transformer/tokenization.py
+"""
+from typing import List
+from collections import OrderedDict
+
+from prenlp.tokenizer import SentencePiece
+
+class Tokenizer:
+    def __init__(self, tokenizer, vocab_file: str,
+                 pad_token: str = '[PAD]',
+                 unk_token: str = '[UNK]',
+                 bos_token: str = '[BOS]',
+                 eos_token: str = '[EOS]',
+                 sep_token: str = '[SEP]',
+                 cls_token: str = '[CLS]',
+                 mask_token: str = '[MASK]'):
+        self.tokenizer = tokenizer
+        self.pad_token = pad_token
+        self.unk_token = unk_token
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+        self.sep_token = sep_token
+        self.cls_token = cls_token
+        self.mask_token = mask_token
+        self.vocab = OrderedDict()
+        self.ids_to_tokens = OrderedDict()
+
+        # Build vocab and ids_to_tokens
+        with open(vocab_file, 'r', encoding='utf-8') as reader:
+            for i, line in enumerate(reader.readlines()):
+                token =line.split()[0]
+                self.vocab[token] = i
+        for token, id in self.vocab.items():
+            self.ids_to_tokens[id] = token
+
+    def tokenize(self, text: str) -> List[str]:
+        """Tokenize given text.
+        """
+        return self.tokenizer(text)
+
+    def convert_token_to_id(self, token: str) -> int:
+        """Convert a token (str) in an id (integer) using the vocab.
+        """
+        return self.vocab.get(token, self.vocab.get(self.unk_token))
+
+    def convert_id_to_token(self, id: int) -> str:
+        """Convert an id (integer) in a token (str) using the vocab.
+        """
+        return self.ids_to_tokens.get(id, self.unk_token)
+
+    def convert_tokens_to_ids(self, tokens: List[str]) -> List[int]:
+        """Convert list of tokens in list of ids using the vocab.
+        """
+        return [self.convert_token_to_id(token) for token in tokens]
+    
+    def convert_ids_to_tokens(self, ids: List[int]) -> List[str]:
+        """Convert list of ids in list of tokens using the vocab.
+        """
+        return [self.convert_id_to_token(id) for id in ids]
+    
+    @property
+    def vocab_size(self) -> int:
+        """Vocabulary size.
+        """
+        return len(self.vocab)
+
+    @property
+    def pad_token_id(self) -> int:
+        """Id of pad_token in the vocab.
+        """
+        return self.convert_token_to_id(self.pad_token)
+    
+    @property
+    def unk_token_id(self) -> int:
+        """Id of unk_token in the vocab.
+        """
+        return self.convert_token_to_id(self.unk_token)
+    
+    @property
+    def bos_token_id(self) -> int:
+        """Id of bos_token in the vocab.
+        """
+        return self.convert_token_to_id(self.bos_token)
+    
+    @property
+    def eos_token_id(self) -> int:
+        """Id of eos_token in the vocab.
+        """
+        return self.convert_token_to_id(self.eos_token)
+    
+    @property
+    def sep_token_id(self) -> int:
+        """Id of sep_token in the vocab.
+        """
+        return self.convert_token_to_id(self.sep_token)
+    
+    @property
+    def cls_token_id(self) -> int:
+        """Id of cls_token in the vocab.
+        """
+        return self.convert_token_to_id(self.cls_token)
+    
+    @property
+    def mask_token_id(self) -> int:
+        """Id of mask_token in the vocab.
+        """
+        return self.convert_token_to_id(self.mask_token)
+
+class PretrainedTokenizer(Tokenizer):
+    def __init__(self, pretrained_model: str, vocab_file: str,
+                 pad_token: str = '[PAD]',
+                 unk_token: str = '[UNK]',
+                 bos_token: str = '[BOS]',
+                 eos_token: str = '[EOS]',
+                 sep_token: str = '[SEP]',
+                 cls_token: str = '[CLS]',
+                 mask_token: str = '[MASK]'):
+        tokenizer = SentencePiece.load(pretrained_model)
+
+        super(PretrainedTokenizer, self).__init__(tokenizer, vocab_file, pad_token, unk_token, bos_token, eos_token)
+
+    def detokenize(self, tokens: List[str]) -> str:
+        """Detokenize given tokens.
+        """
+        return self.tokenizer.detokenize(tokens)
